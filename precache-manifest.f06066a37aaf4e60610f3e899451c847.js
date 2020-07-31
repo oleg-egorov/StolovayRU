78bac46429d34c4ce0f535dc2c2d657a5b507070
Oleg-Egorov <legek99@yandex.ru>
2020-07-31 18:43:00 +0300

    Updates

---
+++ precache-manifest.f06066a37aaf4e60610f3e899451c847.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0790308710e4dfc8ed58de0a87bedd",
+    "url": "/StolovayRU/index.html"
+  },
+  {
+    "revision": "fb9da56c229275cdd56f",
+    "url": "/StolovayRU/static/css/main.ea14746d.chunk.css"
+  },
+  {
+    "revision": "de3ec4651a95f1f117a6",
+    "url": "/StolovayRU/static/js/2.f79c78a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StolovayRU/static/js/2.f79c78a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9da56c229275cdd56f",
+    "url": "/StolovayRU/static/js/main.8a576079.chunk.js"
+  },
+  {
+    "revision": "b622c05c38dbbc8aa9d3",
+    "url": "/StolovayRU/static/js/runtime-main.d071d407.js"
+  },
+  {
+    "revision": "3bd9b6f67875ed4c7b6bc1f06c86b747",
+    "url": "/StolovayRU/static/media/1.3bd9b6f6.png"
+  },
+  {
+    "revision": "a4714f1602b6c88fb19d269c9074436d",
+    "url": "/StolovayRU/static/media/1.a4714f16.png"
+  },
+  {
+    "revision": "5450882f3d36763045ebc299c495e17a",
+    "url": "/StolovayRU/static/media/10.5450882f.png"
+  },
+  {
+    "revision": "79b3946b0f5c566e7616d7cbcfca2720",
+    "url": "/StolovayRU/static/media/11.79b3946b.png"
+  },
+  {
+    "revision": "03ca3c538f0847e9b53d09b14dd26ea5",
+    "url": "/StolovayRU/static/media/2.03ca3c53.png"
+  },
+  {
+    "revision": "7d0d634f7ccfba921c757d8bb6dc7cf3",
+    "url": "/StolovayRU/static/media/2.7d0d634f.png"
+  },
+  {
+    "revision": "4192ab05902fa76a03cf746f296ff9d1",
+    "url": "/StolovayRU/static/media/3.4192ab05.png"
+  },
+  {
+    "revision": "0ba778cf5a864c9b8f4a2c6173fffb34",
+    "url": "/StolovayRU/static/media/4.0ba778cf.png"
+  },
+  {
+    "revision": "efa96ce16918693eeb9db50225a9d742",
+    "url": "/StolovayRU/static/media/5.efa96ce1.png"
+  },
+  {
+    "revision": "daca6a3ad0b22a9b09f386fda3836b31",
+    "url": "/StolovayRU/static/media/6.daca6a3a.png"
+  },
+  {
+    "revision": "41848a23ab458b29a1834a5f274852e3",
+    "url": "/StolovayRU/static/media/7.41848a23.png"
+  },
+  {
+    "revision": "12e32a54dd5b4268524ee3675e04305e",
+    "url": "/StolovayRU/static/media/8.12e32a54.png"
+  },
+  {
+    "revision": "bc9e8896710cd206e638710e1988e75d",
+    "url": "/StolovayRU/static/media/9.bc9e8896.png"
+  },
+  {
+    "revision": "a53ffaec45e3bc49d0f57002ff66b0f1",
+    "url": "/StolovayRU/static/media/abat.a53ffaec.png"
+  },
+  {
+    "revision": "f6242ef58d4ccdc9726022b5ccb1f641",
+    "url": "/StolovayRU/static/media/box.f6242ef5.svg"
+  },
+  {
+    "revision": "36653024a1ad73353bea20338d1984d8",
+    "url": "/StolovayRU/static/media/clients.36653024.png"
+  },
+  {
+    "revision": "6691d100bfda6b96bbfbb9455963ff15",
+    "url": "/StolovayRU/static/media/clients_min.6691d100.png"
+  },
+  {
+    "revision": "8b9fedb535862aa76e25d88f3f34af0f",
+    "url": "/StolovayRU/static/media/email.8b9fedb5.svg"
+  },
+  {
+    "revision": "12ef34a9e2b6cf8fd0997d9d50a95e0c",
+    "url": "/StolovayRU/static/media/hallde.12ef34a9.png"
+  },
+  {
+    "revision": "7fa1ca6a033bb66bee58ba19184ab84a",
+    "url": "/StolovayRU/static/media/hicold.7fa1ca6a.png"
+  },
+  {
+    "revision": "095ac772aecc943d3c9cfa09e9fd3b2e",
+    "url": "/StolovayRU/static/media/logo.095ac772.svg"
+  },
+  {
+    "revision": "ae113205dc59a8974731bf833924d2a1",
+    "url": "/StolovayRU/static/media/logo2.ae113205.png"
+  },
+  {
+    "revision": "485039bc715fb80e75da2f5fc21813a6",
+    "url": "/StolovayRU/static/media/loupe.485039bc.svg"
+  },
+  {
+    "revision": "9dc0dd21253f8f85f3aeffdf40431226",
+    "url": "/StolovayRU/static/media/main.9dc0dd21.png"
+  },
+  {
+    "revision": "d5b486487376f7295854815db1a93224",
+    "url": "/StolovayRU/static/media/myriadpro-bold.d5b48648.ttf"
+  },
+  {
+    "revision": "f4bf8ccd575532aae5e5b07b1749de32",
+    "url": "/StolovayRU/static/media/myriadpro-light.f4bf8ccd.ttf"
+  },
+  {
+    "revision": "3ca455cf81fa4d0e2b353aa74196b088",
+    "url": "/StolovayRU/static/media/myriadpro-regular.3ca455cf.ttf"
+  },
+  {
+    "revision": "9368c15c5076c329f10c6724227db1fa",
+    "url": "/StolovayRU/static/media/next-categories.9368c15c.svg"
+  },
+  {
+    "revision": "a483a59acbb432154a72da5c8bbe1a74",
+    "url": "/StolovayRU/static/media/next.a483a59a.svg"
+  },
+  {
+    "revision": "dba05bc84c069d99d5a2bc831eb80226",
+    "url": "/StolovayRU/static/media/product.dba05bc8.png"
+  },
+  {
+    "revision": "4329d5a5c065e9aec99191d14bfe002c",
+    "url": "/StolovayRU/static/media/rational.4329d5a5.png"
+  },
+  {
+    "revision": "f439e794533160fe240083e5a4ed6ef6",
+    "url": "/StolovayRU/static/media/shopping.f439e794.svg"
+  },
+  {
+    "revision": "521c28bbcb975cce1f11bbb2c08e57e0",
+    "url": "/StolovayRU/static/media/shopping_orange.521c28bb.svg"
+  },
+  {
+    "revision": "d5a8fc61a434fadd250b76b20a90080c",
+    "url": "/StolovayRU/static/media/simeco.d5a8fc61.png"
+  },
+  {
+    "revision": "c57c3724b8b5e0ef7c434b1e305c256e",
+    "url": "/StolovayRU/static/media/slide.c57c3724.png"
+  },
+  {
+    "revision": "393451a43db46353dccbb16b0b2f0b5a",
+    "url": "/StolovayRU/static/media/slider.393451a4.png"
+  },
+  {
+    "revision": "6489413aaea59111bd7c5e239706825f",
+    "url": "/StolovayRU/static/media/telephone.6489413a.svg"
+  },
+  {
+    "revision": "9ee833334ca23fe4d5027ea8b690b399",
+    "url": "/StolovayRU/static/media/unox.9ee83333.png"
+  }
+]);
